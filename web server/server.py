920c74f338de24c76fc5bb6c70c7178c53e464da
Alex Costan <acostan.con@orangetheory.com>
2020-08-02 13:42:35 -0400

    using variable rules for URL parameters

--- web server/server.py
+++ web server/server.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 print(__name__)
 
 
 @app.route('/')
 def hello_world():
+    print(url_for('static', filename='favicon.ico'))
     return render_template('./index.html')
+
+
+@app.route('/<username>')
+def user_name(username=None):
+    return render_template('./index.html', name=username)
+
+
+@app.route('/<username>/<int:post_id>')
+def user_name_and_post_id(username=None, post_id=None):
+    return render_template('./index.html', name=username, post_id=post_id)
 
 
 @app.route('/about.html')
