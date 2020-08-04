1d8dc7b36f53ed336f29409045eac3694f485603
Alex Costan <acostan.con@orangetheory.com>
2020-08-04 12:14:21 -0400

    request data using post method and replied to user after contact form submission

--- web server/server.py
+++ web server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 print(__name__)
 
@@ -15,4 +15,9 @@ def html_page(page_name):
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return 'form submitted hooray!'
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong. Try again.'
